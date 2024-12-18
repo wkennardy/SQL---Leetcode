6af8bc430164243c8fe0840bf433cd367a4aaa21
William Kennardy <williamkennardy@gmail.com>
2024-12-18 15:20:30 +0700

    Time: 717 ms (5%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,5 +3,5 @@ SELECT
     a.id
 FROM Weather AS a
 CROSS JOIN Weather AS b
-WHERE b.temperature < a.temperature AND
+WHERE a.temperature > b.temperature AND
     (a.recordDate - b.recordDate)::integer = 1
